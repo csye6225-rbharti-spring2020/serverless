45e56038becdbe6359e903cdc550f412bdaac954
Rohan Bharti <bharti.r@northeastern.edu>
2020-03-31 17:52:26 -0400

    Added SES Client and configured emailing service

---
+++ src/main/java/AmazonSES.java
@@ -0,0 +1,80 @@
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import com.amazonaws.services.simpleemail.model.Body;
+import com.amazonaws.services.simpleemail.model.Content;
+import com.amazonaws.services.simpleemail.model.Destination;
+import com.amazonaws.services.simpleemail.model.Message;
+import com.amazonaws.services.simpleemail.model.SendEmailRequest;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * AmazonSES Helper class to construct an email and send the email to the respective user's email
+ *
+ * @author rohan_bharti
+ */
+public class AmazonSES {
+
+    private String FROM;
+    private String TO;
+    private String SUBJECT;
+    private String BODY;
+
+    private AmazonSimpleEmailService client;
+
+    /**
+     * Constructor to set up the SES client and message components
+     *
+     * @param FROM
+     * @param TO
+     * @param SUBJECT
+     */
+    public AmazonSES(String FROM, String TO, String SUBJECT, String billsDueUrls) {
+        this.client = AmazonSimpleEmailServiceClientBuilder.standard().withRegion(Regions.US_EAST_1).build();
+        this.FROM = FROM;
+        this.TO = TO;
+        this.SUBJECT = SUBJECT;
+        this.BODY = constructBodyForBillsDue(billsDueUrls);
+    }
+
+    /**
+     * Sends the Email to the user
+     *
+     * @return boolean
+     */
+    public boolean sendEmail() {
+        try {
+            SendEmailRequest request = new SendEmailRequest()
+                    .withDestination(
+                            new Destination().withToAddresses(TO))
+                    .withMessage(new Message()
+                            .withBody(new Body()
+                                    .withText(new Content()
+                                            .withCharset("UTF-8").withData(this.BODY)))
+                            .withSubject(new Content()
+                                    .withCharset("UTF-8").withData(SUBJECT)))
+                    .withSource(FROM);
+            this.client.sendEmail(request);
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
+    /**
+     * Constructs the Html message String Body for the email. The parameter is a String containing List of Bill JSON object values.
+     *
+     * @param billsDueString
+     */
+    private String constructBodyForBillsDue(String billsDueString) {
+        List<String> billsDueUrlsList = Stream.of(billsDueString.split(",", -1)).collect(Collectors.toList());
+        StringBuilder bodyStringBuilder = new StringBuilder();
+        for(String billDueUrl: billsDueUrlsList) {
+            bodyStringBuilder.append(billDueUrl + "\n");
+        }
+        return bodyStringBuilder.toString();
+    }
+}
---
+++ src/main/java/Handler.java
@@ -0,0 +1,42 @@
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Map;
+
+/**
+ * Handler class to receive SNS Topic messages and send an email to the user.
+ */
+public class Handler implements RequestHandler<SNSEvent, Object> {
+
+    private final String SNS_EMAIL_KEY = "userEmail";
+    private final String SNS_BILLS_DUE_LIST_KEY = "billsDueUrlsListString";
+    private final String FROM_EMAIL = "no-reply@amazon.ses";
+    private final String SUBJECT_EMAIL = "Bills Due Update";
+
+    private AmazonSES amazonSESClient;
+
+    public Object handleRequest(SNSEvent snsEvent, Context context) {
+
+        String timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(Calendar.getInstance().getTime());
+        context.getLogger().log("Handler Invocation started: " + timeStamp);
+
+        Map<String, SNSEvent.MessageAttribute> stringMessageAttributeMap = snsEvent.getRecords().get(0).getSNS().getMessageAttributes();
+        String userEmail = stringMessageAttributeMap.get(SNS_EMAIL_KEY).getValue();
+        String billsDueListString = stringMessageAttributeMap.get(SNS_BILLS_DUE_LIST_KEY).getValue();
+
+        amazonSESClient = new AmazonSES(FROM_EMAIL, userEmail, SUBJECT_EMAIL, billsDueListString);
+        boolean emailSent = amazonSESClient.sendEmail();
+
+        if(emailSent) {
+            context.getLogger().log("Email sent to " + userEmail + " successfully containing all the due Bills Urls");
+        }
+
+        timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(Calendar.getInstance().getTime());
+        context.getLogger().log("Handler Invocation completed: " + timeStamp);
+        return null;
+    }
+}
+
